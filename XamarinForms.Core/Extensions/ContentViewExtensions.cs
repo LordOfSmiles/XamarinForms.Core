0aa12ff0b3a76bc54fc85bd1b9df6cf3331786d7
PGoncharov <g-p@live.ru>
2017-09-27 21:53:28 +0200

    Updated Xamarin.Forms nuget

--- XamarinForms.Core/Extensions/ContentViewExtensions.cs
+++ XamarinForms.Core/Extensions/ContentViewExtensions.cs
@@ -9,6 +9,22 @@ namespace XamarinForms.Core.Extensions
 {
     public static class ContentViewExtensions
     {
+        public static void OnCustomIdiom(this Page page, Action phoneCustomizations, Action tabletCustomizations = null, Action desktopCustomizations = null)
+        {
+            switch (Device.Idiom)
+            {
+                case TargetIdiom.Phone:
+                    phoneCustomizations?.Invoke();
+                    break;
+                case TargetIdiom.Tablet:
+                    tabletCustomizations?.Invoke();
+                    break;
+                case TargetIdiom.Desktop:
+                    desktopCustomizations?.Invoke();
+                    break;
+            }
+        }
+
         public static void OnCustomIdiom(this ContentView view, Action phoneCustomizations, Action tabletCustomizations = null, Action desktopCustomizations = null)
         {
             switch (Device.Idiom)
@@ -35,7 +51,7 @@ namespace XamarinForms.Core.Extensions
                 case Device.iOS:
                     onIos?.Invoke();
                     break;
-                case Device.Windows:
+                case Device.UWP:
                     onUwp?.Invoke();
                     break;
             }
