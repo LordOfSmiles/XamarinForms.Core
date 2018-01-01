3bb093d27fa554b15c84e98ba73656a04a3c884c
Pavel Goncharov <g-p@live.ru>
2018-01-01 09:19:53 +0200

    Added numericValidationBehavior

---
+++ XamarinForms.Core.Standard/Behaviors/NumericValidationBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace XamarinForms.Core.Standard.Behaviors
+{
+    public class NumericValidationBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private static void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            var entry = sender as Entry;
+            if (entry == null)
+                return;
+
+            if (!string.IsNullOrWhiteSpace(args.NewTextValue))
+            {
+                bool isValid = args.NewTextValue.ToCharArray().All(char.IsDigit);
+
+                entry.Text = isValid
+                    ? args.NewTextValue
+                    : args.NewTextValue.Remove(args.NewTextValue.Length - 1);
+            }
+        }
+    }
+}
