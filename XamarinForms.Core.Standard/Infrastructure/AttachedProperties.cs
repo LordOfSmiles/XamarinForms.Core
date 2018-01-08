f49f65514bc80fff10cb390cc3ef7fafc929a2ad
Pavel Goncharov <g-p@live.ru>
2018-01-08 10:40:02 +0200

    Localization

---
+++ XamarinForms.Core.Standard/Infrastructure/AttachedProperties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinForms.Core.Standard.Infrastructure
+{
+    public static class AttachedProperties
+    {
+        public static readonly BindableProperty TagProperty = BindableProperty.CreateAttached("Tag", typeof(object), typeof(AttachedProperties), null);
+
+        public static object GetTag(BindableObject bo)
+        {
+            return bo.GetValue(TagProperty);
+        }
+
+        public static void SetTag(BindableObject bo, object value)
+        {
+            bo.SetValue(TagProperty, value);
+        }
+    }
+}
---
+++ XamarinForms.Core.Standard/Infrastructure/Interfaces/ILocalize.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+
+namespace XamarinForms.Core.Standard.Infrastructure.Interfaces
+{
+    public interface ILocalize
+    {
+        CultureInfo GetCurrentCultureInfo();
+        void SetLocale(CultureInfo ci);
+    }
+}
