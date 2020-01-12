5a7e39fbc9cbac4d65962298d3f3f084da6af3fe
LordOfSmiles <g-p@live.ru>
2020-01-12 06:31:05 +0200

    Added HeaderControl.TextInputProperty

--- XamarinForms.Core/Controls/HeaderControl.cs
+++ XamarinForms.Core/Controls/HeaderControl.cs
@@ -18,106 +18,39 @@ namespace XamarinForms.Core.Standard.Controls
 
         #region Bindable proeprties
 
-        // #region Text
-        //
-        // public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(HeaderControl), string.Empty, propertyChanged: OnTextChanged);
-        //
-        // public string Text
-        // {
-        //     get => (string)GetValue(TextProperty);
-        //     set => SetValue(TextProperty, value);
-        // }
-        //
-        // private static void OnTextChanged(BindableObject bo, object oldValue, object newValue)
-        // {
-        //     var ctrl = bo as HeaderControl;
-        //     if (ctrl == null)
-        //         return;
-        //
-        //     switch (Device.RuntimePlatform)
-        //     {
-        //         case Device.Android:
-        //             ctrl._lblHeader.Text = newValue?.ToString() ?? "";
-        //             break;
-        //         case Device.iOS:
-        //             ctrl._lblHeader.Text = newValue?.ToString().ToUpper() ?? "";
-        //             break;
-        //     }
-        // }
-        //
-        // #endregion
-        //
-        // #region TextColor
-        //
-        // public static readonly BindableProperty TextColorProperty = BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(HeaderControl), Color.Default, propertyChanged: OnTextColorChanged);
-        //
-        // public Color TextColor
-        // {
-        //     get => (Color)GetValue(TextColorProperty);
-        //     set => SetValue(TextColorProperty, value);
-        // }
-        //
-        // private static void OnTextColorChanged(BindableObject bindable, object oldValue, object newValue)
-        // {
-        //     var ctrl = bindable as HeaderControl;
-        //     if (ctrl == null)
-        //         return;
-        //
-        //     if (ctrl._lblHeader == null)
-        //         return;
-        //
-        //     ctrl._lblHeader.TextColor = (Color)newValue;
-        // }
-        //
-        // #endregion
-        //
-        // #region FontSize
-        //
-        // public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(double), typeof(HeaderControl), FontHelper.LabelSmall, propertyChanged: OnFontSizeChanged);
-        //
-        // public double FontSize
-        // {
-        //     get => (double)GetValue(FontSizeProperty);
-        //     set => SetValue(FontSizeProperty, value);
-        // }
-        //
-        // private static void OnFontSizeChanged(BindableObject bindable, object oldValue, object newValue)
-        // {
-        //     var ctrl = bindable as HeaderControl;
-        //     if (ctrl == null)
-        //         return;
-        //
-        //     ctrl._lblHeader.FontSize = (double)newValue;
-        // }
-        //
-        // #endregion
-
-        // #region HeaderPadding
-        //
-        // public static readonly BindableProperty HeaderPaddingProperty = BindableProperty.Create(nameof(HeaderPadding), 
-        //     typeof(Thickness), 
-        //     typeof(HeaderControl),
-        //     DeviceHelper.OnPlatform(new Thickness(16,6,8,6),new Thickness(16,8,8,8)), 
-        //     propertyChanged: OnHeaderPaddingChanged);
-        //
-        // public Thickness HeaderPadding
-        // {
-        //     get => (Thickness)GetValue(HeaderPaddingProperty);
-        //     set => SetValue(HeaderPaddingProperty, value);
-        // }
-        //
-        // private static void OnHeaderPaddingChanged(BindableObject bindable, object oldValue, object newValue)
-        // {
-        //     var ctrl = bindable as HeaderControl;
-        //     if (ctrl == null)
-        //         return;
-        //
-        //
-        //     ctrl.Padding = (Thickness) newValue;
-        // }
-        //
-        // #endregion
-
+        #region TextInput
+        
+        public static readonly BindableProperty TextInputProperty = BindableProperty.Create(nameof(TextInput), 
+            typeof(string),
+            typeof(HeaderControl),
+            string.Empty, 
+            propertyChanged: OnTextInputChanged);
+        
+        public string TextInput
+        {
+            get => (string)GetValue(TextInputProperty);
+            set => SetValue(TextInputProperty, value);
+        }
+        
+        private static void OnTextInputChanged(BindableObject bo, object oldValue, object newValue)
+        {
+            var ctrl = bo as HeaderControl;
+            if (ctrl == null)
+                return;
+        
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    ctrl.Text = newValue?.ToString() ?? "";
+                    break;
+                case Device.iOS:
+                    ctrl.Text = newValue?.ToString().ToUpper() ?? "";
+                    break;
+            }
+        }
+        
+        #endregion
+        
         #endregion
     }
 }
