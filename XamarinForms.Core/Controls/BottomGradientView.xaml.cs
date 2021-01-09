0e6b0504f32d497252f757d0d3776dbef896f666
LordOfSmiles <g-p@live.ru>
2021-01-09 12:53:12 +0200

    Refactored

--- XamarinForms.Core/Controls/BottomGradientView.xaml.cs
+++ XamarinForms.Core/Controls/BottomGradientView.xaml.cs
@@ -1,3 +1,5 @@
+using Xamarin.Forms;
+
 namespace BabyDream.Infrastructure.Controls
 {
     public partial class BottomGradientView
@@ -6,5 +8,39 @@ namespace BabyDream.Infrastructure.Controls
         {
             InitializeComponent();
         }
+        
+        #region Bindable Properties
+        
+        #region StartColor
+
+        public static readonly BindableProperty StartColorProperty = BindableProperty.Create(nameof(StartColor),
+            typeof(Color),
+            typeof(BottomGradientView),
+            Color.Default);
+
+        public Color StartColor
+        {
+            get => (Color) GetValue(StartColorProperty);
+            set => SetValue(StartColorProperty, value);
+        }
+
+        #endregion
+        
+        #region EndColor
+
+        public static readonly BindableProperty EndColorProperty = BindableProperty.Create(nameof(EndColor),
+            typeof(Color),
+            typeof(BottomGradientView),
+            Color.Default);
+
+        public Color EndColor
+        {
+            get => (Color) GetValue(EndColorProperty);
+            set => SetValue(EndColorProperty, value);
+        }
+
+        #endregion
+
+        #endregion
     }
 }
--- XamarinForms.Core/Controls/TopGradientView.xaml.cs
+++ XamarinForms.Core/Controls/TopGradientView.xaml.cs
@@ -1,3 +1,5 @@
+using Xamarin.Forms;
+
 namespace BabyDream.Infrastructure.Controls
 {
     public partial class TopGradientView 
@@ -6,5 +8,39 @@ namespace BabyDream.Infrastructure.Controls
         {
             InitializeComponent();
         }
+        
+        #region Bindable Proeprties
+        
+        #region StartColor
+
+        public static readonly BindableProperty StartColorProperty = BindableProperty.Create(nameof(StartColor),
+            typeof(Color),
+            typeof(TopGradientView),
+            Color.Default);
+
+        public Color StartColor
+        {
+            get => (Color) GetValue(StartColorProperty);
+            set => SetValue(StartColorProperty, value);
+        }
+
+        #endregion
+        
+        #region EndColor
+
+        public static readonly BindableProperty EndColorProperty = BindableProperty.Create(nameof(EndColor),
+            typeof(Color),
+            typeof(TopGradientView),
+            Color.Default);
+
+        public Color EndColor
+        {
+            get => (Color) GetValue(EndColorProperty);
+            set => SetValue(EndColorProperty, value);
+        }
+
+        #endregion
+        
+        #endregion
     }
 }
