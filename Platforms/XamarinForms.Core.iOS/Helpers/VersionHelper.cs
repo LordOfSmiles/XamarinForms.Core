1c4fbfac2a3fdd0c552b687f005d7d8b9607ac79
LordOfSmiles <g-p@live.ru>
2021-03-15 22:14:10 +0200

    Add ios 14.5

--- Platforms/XamarinForms.Core.iOS/Helpers/VersionHelper.cs
+++ Platforms/XamarinForms.Core.iOS/Helpers/VersionHelper.cs
@@ -6,5 +6,6 @@ namespace XamarinForms.Core.iOS.Helpers
     public static class VersionHelper
     {
         public static bool IsOs13OrHigher => UIDevice.CurrentDevice.CheckSystemVersion(13, 0);
+        public static bool IsOs14_5_OrHigher => UIDevice.CurrentDevice.CheckSystemVersion(14, 5);
     }
 }
