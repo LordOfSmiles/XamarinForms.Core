8ea6a58c4f1f4ad1ee0afbd5c824e0e02615f2e9
LordOfSmiles <g-p@live.ru>
2021-04-24 19:24:33 +0200

    Refactored

--- XamarinForms.Core/PlatformServices/IToastPlatformService.cs
+++
@@ -1,8 +0,0 @@
-namespace XamarinForms.Core.PlatformServices
-{
-    public interface IToastPlatformService
-    {
-        void ShowAlert(string title, string message);
-        void ShowAlert(string message);
-    }
-}
