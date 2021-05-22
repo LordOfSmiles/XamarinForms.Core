146f92554fcee8e6ca94fae0fb4351ffb990c60b
LordOfSmiles <g-p@live.ru>
2021-05-22 06:14:38 +0200

    +

--- Platforms/XamarinForms.Core.iOS/Helpers/VersionHelper.cs
+++ Platforms/XamarinForms.Core.iOS/Helpers/VersionHelper.cs
@@ -6,6 +6,7 @@ namespace XamarinForms.Core.iOS.Helpers
     {
         public static bool IsOs13OrHigher => UIDevice.CurrentDevice.CheckSystemVersion(13, 0);
         public static bool IsOs14_4_OrHigher => UIDevice.CurrentDevice.CheckSystemVersion(14, 4);
+        public static bool IsOs14_5_OrHigher => UIDevice.CurrentDevice.CheckSystemVersion(14, 5);
 
         public static bool IsVersionEqualsOrHigher(int major, int minor) => UIDevice.CurrentDevice.CheckSystemVersion(major, minor);
     }
