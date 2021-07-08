d077c9e30e9ebaf3fe0135cb0787fba49eb26b79
LordOfSmiles <g-p@live.ru>
2021-07-08 07:21:23 +0200

    Added PopupViewModelBase

---
+++ XamarinForms.Core/ViewModels/PopupViewModelBase.cs
@@ -0,0 +1,15 @@
+using Xamarin.Core.Models;
+
+namespace XamarinForms.Core.ViewModels
+{
+    public abstract class PopupViewModelBase:NotifyObject
+    {
+
+        public bool IsConfirmed
+        {
+            get => _isConfirmed;
+            protected set => SetProperty(ref _isConfirmed, value);
+        }
+        private bool _isConfirmed;
+    }
+}
