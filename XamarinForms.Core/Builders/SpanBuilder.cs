0f7c22bcb96420bcb90bb1622c0ca2bd6ba81f0d
LordOfSmiles <g-p@live.ru>
2021-07-24 07:04:16 +0200

    Added SpanBuilder

---
+++ XamarinForms.Core/Builders/SpanBuilder.cs
@@ -0,0 +1,45 @@
+using System;
+using Xamarin.Forms;
+
+namespace XamarinForms.Core.Builders
+{
+    public class SpanBuilder
+    {
+        public Span GetSpan()
+        {
+            return _span;
+        }
+
+        public SpanBuilder WithBold()
+        {
+            _span.FontAttributes = FontAttributes.Bold;
+            return this;
+        }
+
+        public SpanBuilder SetFontSize(double fontSize)
+        {
+            _span.FontSize = fontSize;
+            return this;
+        }
+
+        public SpanBuilder SetTextColor(Color lightColor, Color darkColor)
+        {
+            _span.SetAppThemeColor(Span.TextColorProperty, lightColor, darkColor);
+            return this;
+        }
+        
+        #region Fields
+
+        private readonly Span _span;
+
+        #endregion
+
+        public SpanBuilder(string text)
+        {
+            _span = new Span()
+            {
+                Text = text
+            };
+        }
+    }
+}
