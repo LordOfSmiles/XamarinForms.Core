ba6a8d8be8ae8f7990a99c8da0d7b1af543aa469
LordOfSmiles <g-p@live.ru>
2021-09-20 22:37:53 +0200

    [nuget] Xamarin.Forms 5 SR5, sqlite

--- XamarinForms.Core/Views/ShellContentPage.cs
+++ XamarinForms.Core/Views/ShellContentPage.cs
@@ -5,14 +5,16 @@ using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using XamarinForms.Core.Infrastructure.Navigation;
 using XamarinForms.Core.ViewModels;
+using FlyoutPage = Xamarin.Forms.FlyoutPage;
 
 namespace XamarinForms.Core.Views
 {
-    public abstract class ShellContentPage : ContentPage
+    public class ShellContentPage : ContentPage
     {
         protected override async void OnAppearing()
         {
             var vm = BindingContext as ViewModelBase;
+
             if (vm != null)
             {
                 try
@@ -37,7 +39,43 @@ namespace XamarinForms.Core.Views
             base.OnDisappearing();
         }
 
-        protected ShellContentPage()
+        public ShellContentPage()
+        {
+            On<iOS>().SetUseSafeArea(true);
+        }
+    }
+
+    public abstract class ShellFlyoutPage : FlyoutPage
+    {
+        protected override async void OnAppearing()
+        {
+            var vm = BindingContext as ViewModelBase;
+
+            if (vm != null)
+            {
+                try
+                {
+                    await vm.OnAppearingAsync(NavigationHelper.GetParametersByPageType(GetType()));
+                }
+                catch (Exception ex)
+                {
+                    var logger = DependencyService.Get<ICrashlyticsService>();
+                    logger?.TrackError(ex);
+                }
+            }
+
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            var vm = BindingContext as ViewModelBase;
+            vm?.OnDisappearing();
+
+            base.OnDisappearing();
+        }
+
+        protected ShellFlyoutPage()
         {
             On<iOS>().SetUseSafeArea(true);
         }
