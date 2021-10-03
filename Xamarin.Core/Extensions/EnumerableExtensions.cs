15eb2c204e5c0a3c8d449b9028c278db4dfc5b6a
LordOfSmiles <g-p@live.ru>
2021-10-03 19:10:00 +0200

    Added IEnumerableExtension

---
+++ Xamarin.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xamarin.Core.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Distinct method that accepts a perdicate
+        /// </summary>
+        /// <typeparam name="TSource">The type of the t source.</typeparam>
+        /// <typeparam name="TKey">The type of the t key.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>IEnumerable&lt;TSource&gt;.</returns>
+        /// <exception cref="System.ArgumentNullException">source</exception>
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source.GroupBy(predicate)
+                         .Select(x => x.First());
+        }
+    }
+}
