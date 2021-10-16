46379ea3bb2cb267090f2b0a94ae36ac7287fb33
LordOfSmiles <g-p@live.ru>
2021-10-16 06:23:58 +0200

    Auto stash before merge of "master" and "origin/master"

--- XamarinForms.Core/Builders/SpanBuilder.cs
+++ XamarinForms.Core/Builders/SpanBuilder.cs
@@ -52,5 +52,11 @@ namespace XamarinForms.Core.Builders
                 Text = text
             };
         }
+
+
+        public static Span NewLine(double fontSize = 2.0) =>
+            new SpanBuilder(Environment.NewLine)
+                .SetFontSize(fontSize)
+                .GetSpan();
     }
 }
