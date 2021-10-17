a81feb1b77a14f71d107ca20f52f66c75423392f
LordOfSmiles <g-p@live.ru>
2021-10-17 07:54:21 +0200

    [Added] Тестовый Id для рекламы

--- XamarinForms.Core/Controls/AdvBanner.cs
+++ XamarinForms.Core/Controls/AdvBanner.cs
@@ -6,6 +6,12 @@ namespace XamarinForms.Core.Controls
     {
         public AdvBanner(string adUnitId, Color colorLight, Color colorDark)
         {
+            #if DEBUG
+
+            adUnitId = "ca-app-pub-3940256099942544/6300978111";
+            
+            #endif
+            
             AdUnitId = adUnitId;
 
             this.SetAppThemeColor(BackgroundColorProperty, colorLight, colorDark);
