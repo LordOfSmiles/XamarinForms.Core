06144ca5f1f530a673d11a1802f6b67942013f88
LordOfSmiles <g-p@live.ru>
2021-11-30 21:58:36 +0200

    [Added] CustomScrollView

---
+++ XamarinForms.Core/Controls/CustomScrollView.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms.Core.Controls
+{
+    public sealed class CustomScrollView:ScrollView
+    {
+        public CustomScrollView()
+        {
+            Scrolled += OnScrolled;
+        }
+
+        private CancellationTokenSource _cts;
+
+        private bool _isTimerEnabled;
+
+        private async void OnScrolled(object sender, ScrolledEventArgs e)
+        {
+            _cts?.Cancel();
+            _cts = new CancellationTokenSource();
+
+            try
+            {
+                await Task.Delay(500, _cts.Token);
+
+                _cts.Token.ThrowIfCancellationRequested();
+                ScrollEnded?.Invoke(this, EventArgs.Empty);
+            }
+            catch
+            {
+                
+            }
+        }
+
+        #region Events
+
+        public event EventHandler ScrollEnded;
+
+        #endregion
+    }
+}
