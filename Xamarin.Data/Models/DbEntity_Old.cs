f22afcb716721e0a0a8375c111817e5529a80535
LordOfSmiles <g-p@live.ru>
2022-02-11 06:36:51 +0200

    Новый класс для моделей SQlite

---
+++ Xamarin.Data/Models/DbEntity_Old.cs
@@ -0,0 +1,18 @@
+using System;
+using SQLite;
+
+namespace Xamarin.Data.Models
+{
+    [Obsolete]
+    public abstract class DbEntity_Old
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+    }
+    
+    public abstract class DbEntity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int DbId { get; set; }
+    }
+}
