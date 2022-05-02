775e9803f7408ec0ec26d6843a333cd853959418
LordOfSmiles <g-p@live.ru>
2022-05-02 07:14:58 +0200

    DoubleHelper and PagingResult

---
+++ Xamarin.Core/Helpers/DoubleHelper.cs
@@ -0,0 +1,12 @@
+namespace Xamarin.Core.Helpers
+{
+    public static class DoubleHelper
+    {
+        public static double Max(double a, double b)
+        {
+            return a > b
+                ? a
+                : b;
+        }
+    }
+}
---
+++ Xamarin.Data/Models/PagingResult.cs
@@ -0,0 +1,15 @@
+namespace Xamarin.Data.Models
+{
+    public sealed class PagingResult<T>
+    {
+        public PagingResult(T[] items, int total)
+        {
+            Items = items;
+            Total = total;
+        }
+
+        public T[] Items { get; }
+        
+        public int Total { get; }
+    }
+}
