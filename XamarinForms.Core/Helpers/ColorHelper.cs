e13ddce74236579fdbcab0509bd98d83c22f9f69
LordOfSmiles <g-p@live.ru>
2022-08-12 22:40:20 +0200

    +

--- XamarinForms.Core/Helpers/ColorHelper.cs
+++ XamarinForms.Core/Helpers/ColorHelper.cs
@@ -5,6 +5,18 @@ namespace XamarinForms.Core.Helpers;
 
 public static class ColorHelper
 {
+    public static bool IsColorsEquals(Color color1, Color color2)
+    {
+        return color1.ToHex() == color2.ToHex();
+    }
+
+    public static Color GetColorByTheme(Color lightColor, Color darkColor)
+    {
+        return ThemeHelper.IsDarkTheme
+            ? darkColor
+            : lightColor;
+    }
+
     public static Color GetTextColorForBackground(Color background, Color textColorForLightBackground, Color textColorForDarkBackground)
     {
         return IsLightColor(background)
@@ -14,12 +26,13 @@ public static class ColorHelper
 
     public static bool IsLightColor(Color background)
     {
-        var r = background.GetByteRed();
-        var g = background.GetByteGreen();
-        var b = background.GetByteBlue();
+        // var r = background.GetByteRed();
+        // var g = background.GetByteGreen();
+        // var b = background.GetByteBlue();
+        //
+        // var luminosity = ((r * 0.299) + (g * 0.587) + (b * 0.114)) / 255;
 
-        var luminosity = ((r * 0.299) + (g * 0.587) + (b * 0.114)) / 255;
-
-        return background.Luminosity >= 0.5;
+        //return background.Luminosity >= 0.5;
+        return !background.IsDark();
     }
 }
---
+++ XamarinForms.Core/Helpers/ThemeHelper.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms;
+
+namespace XamarinForms.Core.Helpers;
+
+public static class ThemeHelper
+{
+    public static bool IsDarkTheme => Application.Current.RequestedTheme == OSAppTheme.Dark;
+}
