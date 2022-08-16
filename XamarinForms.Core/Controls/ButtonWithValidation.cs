bcdc91af9fe97a1fd8f85203c3cadc3ca0e64fc5
LordOfSmiles <g-p@live.ru>
2022-08-16 21:48:54 +0200

    Новая реализация ButtonWithValidation

--- XamarinForms.Core/Controls/ButtonWithValidation.cs
+++ XamarinForms.Core/Controls/ButtonWithValidation.cs
@@ -2,20 +2,22 @@ using Xamarin.Forms;
 
 namespace XamarinForms.Core.Controls;
 
-public sealed class ButtonWithValidation:Button
+public sealed class ButtonWithValidation : Button
 {
     public ButtonWithValidation()
     {
-        Style = InvalidStyle;
+        BackgroundColor = NormalColor;
+        TextColor = NormalTextColor;
     }
-    
+
     #region Bindable Properties
 
     #region IsValid
 
     public static readonly BindableProperty IsValidProperty = BindableProperty.Create(nameof(IsValid),
         typeof(bool),
-        typeof(ButtonWithValidation), false,
+        typeof(ButtonWithValidation),
+        true,
         propertyChanged: OnIsValidChanged);
 
     public bool IsValid
@@ -29,37 +31,72 @@ public sealed class ButtonWithValidation:Button
         var ctrl = (ButtonWithValidation)bindable;
 
         var isValid = (bool)newValue;
-        ctrl.Style = isValid
-            ? ctrl.ValidStyle 
-            : ctrl.InvalidStyle;
+        if (isValid)
+        {
+            ctrl.BackgroundColor = ctrl.NormalColor;
+            ctrl.TextColor = ctrl.NormalTextColor;
+        }
+        else
+        {
+            ctrl.BackgroundColor = ctrl.InvalidColor;
+            ctrl.TextColor = ctrl.InvalidTextColor;
+        }
     }
 
     #endregion
-    
-    #region ValidStyle
 
-    public static readonly BindableProperty ValidStyleProperty = BindableProperty.Create(nameof(ValidStyle),
-        typeof(Style),
+    #region NormalColor
+
+    public static readonly BindableProperty NormalColorProperty = BindableProperty.Create(nameof(NormalColor),
+        typeof(Color),
         typeof(ButtonWithValidation));
 
-    public Style ValidStyle
+    public Color NormalColor
     {
-        get => (Style)GetValue(ValidStyleProperty);
-        set => SetValue(ValidStyleProperty, value);
+        get => (Color)GetValue(NormalColorProperty);
+        set => SetValue(NormalColorProperty, value);
     }
 
     #endregion
-    
-    #region InvalidStyle
 
-    public static readonly BindableProperty InvalidStyleProperty = BindableProperty.Create(nameof(InvalidStyle),
-        typeof(Style),
+    #region NormalTextColor
+
+    public static readonly BindableProperty NormalTextColorProperty = BindableProperty.Create(nameof(NormalTextColor),
+        typeof(Color),
         typeof(ButtonWithValidation));
 
-    public Style InvalidStyle
+    public Color NormalTextColor
     {
-        get => (Style)GetValue(InvalidStyleProperty);
-        set => SetValue(InvalidStyleProperty, value);
+        get => (Color)GetValue(NormalTextColorProperty);
+        set => SetValue(NormalTextColorProperty, value);
+    }
+
+    #endregion
+
+    #region InvalidColor
+
+    public static readonly BindableProperty InvalidColorProperty = BindableProperty.Create(nameof(InvalidColor),
+        typeof(Color),
+        typeof(ButtonWithValidation));
+
+    public Color InvalidColor
+    {
+        get => (Color)GetValue(InvalidColorProperty);
+        set => SetValue(InvalidColorProperty, value);
+    }
+
+    #endregion
+
+    #region InvalidTextColor
+
+    public static readonly BindableProperty InvalidTextColorProperty = BindableProperty.Create(nameof(InvalidTextColor),
+        typeof(Color),
+        typeof(ButtonWithValidation));
+
+    public Color InvalidTextColor
+    {
+        get => (Color)GetValue(InvalidTextColorProperty);
+        set => SetValue(InvalidTextColorProperty, value);
     }
 
     #endregion
