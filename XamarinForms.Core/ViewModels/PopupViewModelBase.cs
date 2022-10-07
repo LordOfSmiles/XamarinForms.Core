80fe625e94805d9409caa5f39099501b551d2afe
LordOfSmiles <g-p@live.ru>
2022-10-07 06:53:19 +0200

    +

--- XamarinForms.Core/ViewModels/PopupViewModelBase.cs
+++ XamarinForms.Core/ViewModels/PopupViewModelBase.cs
@@ -5,6 +5,13 @@ namespace XamarinForms.Core.ViewModels;
 public abstract class PopupViewModelBase<T> : NotifyObject
 {
     public abstract T GetResult();
-    
+
     public virtual bool IsModified { get; }
+
+    public bool InputTransparent
+    {
+        get => _inputTransparent;
+        protected set => SetProperty(ref _inputTransparent, value);
+    }
+    private bool _inputTransparent;
 }
