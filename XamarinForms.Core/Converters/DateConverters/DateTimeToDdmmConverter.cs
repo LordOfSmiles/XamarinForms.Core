63032007e6e944e99039d09e5f5ff63ddb603251
LordOfSmiles <g-p@live.ru>
2023-02-02 13:41:27 +0600

    [NEW] DateConverter

---
+++ XamarinForms.Core/Converters/DateConverters/DateTimeToDdmmConverter.cs
@@ -0,0 +1,6 @@
+namespace XamarinForms.Core.Converters.DateConverters;
+
+public sealed class DateTimeToDdmmConverter : DateTimeConverterBase
+{
+    protected override string DateFormat => "dd.MM";
+}
