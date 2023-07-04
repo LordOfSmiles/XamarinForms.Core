ae14a71e9e33225453583cf78e278fd28d82038e
LordOfSmiles <g-p@live.ru>
2023-07-04 21:50:52 +0200

    +

--- XamarinForms.Core/Helpers/PageHelper.cs
+++ XamarinForms.Core/Helpers/PageHelper.cs
@@ -26,7 +26,7 @@ public static class PageHelper
 
             if (!page.On<iOS>().UsingSafeArea())
             {
-                bottom = 50;
+                bottom += (int)page.On<iOS>().SafeAreaInsets().Bottom;
             }
         }
 
