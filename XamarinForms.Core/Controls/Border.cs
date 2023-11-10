0bea5e2290ad12062823e6b40d036129fdccdcbe
LordOfSmiles <g-p@live.ru>
2023-11-10 09:30:51 +0200

    +

--- XamarinForms.Core/Controls/Border.cs
+++ XamarinForms.Core/Controls/Border.cs
@@ -10,3 +10,15 @@ public class Border : CustomFrame
         BackgroundColor = Color.Transparent;
     }
 }
+
+public class BorderOld : Frame
+{
+    public BorderOld()
+    {
+        CornerRadius = 0;
+        HasShadow = false;
+        Padding = new Thickness(0);
+        IsClippedToBounds = true;
+        BackgroundColor = Color.Transparent;
+    }
+}
