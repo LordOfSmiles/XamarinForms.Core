d18cb07355905b8d7c764501893dbd0c72a581bb
LordOfSmiles <g-p@live.ru>
2023-11-12 07:43:15 +0200

    +

--- Xamarin.Core/DateFormats.cs
+++ Xamarin.Core/DateFormats.cs
@@ -3,9 +3,11 @@ namespace Xamarin.Core;
 public static class DateFormats
 {
     public static string dd_MMMM => string.Intern("dd MMMM");
+    public static string dd_MMMM_yyyy => string.Intern("dd MMMM yyyy");
     public static string dd_MMM => string.Intern("dd MMM");
     public static string ddMMyyyy => string.Intern("dd.MM.yyyy");
     public static string ddMMyy => string.Intern("dd.MM.yy");
+    public static string dddd => string.Intern("dddd");
 }
 
 public static class TimeFormats
